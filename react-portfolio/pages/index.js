481a62a01e7353f09ea8c57246dd679243b02f77
Nico <nico@alwaysmarbella.com>
2022-12-04 23:27:05 +0100

    Added main info section
    Added h1,h2, description social buttons and image

--- react-portfolio/pages/index.js
+++ react-portfolio/pages/index.js
@@ -1,5 +1,8 @@
-import Head from 'next/head'
-import {BsFillMoonStarsFill} from 'react-icons/bs'
+import Head from 'next/head';
+import {BsFillMoonStarsFill} from 'react-icons/bs';
+import {AiFillGithub, AiFillLinkedin, AiFillInstagram} from 'react-icons/ai';
+import Image from 'next/image';
+import deved from '../public/dev-ed-wave.png';
 
 export default function Home() {
   return (
@@ -19,6 +22,20 @@ export default function Home() {
               <li><a className="bg-gradient-to-r from-cyan-500 to-teal-500  text-white px-4 py-2 rounded-md ml-8 font-roboto font-light" href="#">Resume</a></li>
             </ul>
           </nav>
+          <div className="text-center p -10">
+            <h2 className="text-5xl py-2 text-teal-600 font-light">Nico Volo</h2>
+            <h3 className="text-2xl py-2 text-gray-600">Frontend Developer</h3>
+            <p className="text-md py-5 leading-8 text-gray-800">Hello there! I'm Nico, a Junior Web Developer in both frontend and backend development, thirsty for learning and passionate about web technologies and building interesting projects. I love learning every day how to create open source projects and contribute to the community.
+</p>
+          </div>
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+            <a className="hover:text-teal-500" href="#"><AiFillGithub/></a>
+            <a className="hover:text-teal-500" href="#"><AiFillLinkedin/></a>
+            <a className="hover:text-teal-500" href="#"><AiFillInstagram/></a>
+          </div>
+          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden ">
+            <Image src={deved} layout="fill" objectFit="cover"/>
+          </div>
         </section>
       </main> 
     </div>
