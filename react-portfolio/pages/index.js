0d60a9386681eda2bf3a6baf3dad631779d89130
Nico <nico@alwaysmarbella.com>
2022-12-06 16:15:47 +0100

    portfolio and large screen
    Added portfolio
    Added large screen adaptation

--- react-portfolio/pages/index.js
+++ react-portfolio/pages/index.js
@@ -6,7 +6,12 @@ import deved from '../public/dev-ed-wave.png';
 import design from '../public/design.png';
 import code from '../public/code.png';
 import consulting from '../public/consulting.png';
-
+import web1 from '../public/web1.png';
+import web2 from '../public/web2.png';
+import web3 from '../public/web3.png';
+import web4 from '../public/web4.png';
+import web5 from '../public/web5.png';
+import web6 from '../public/web6.png';
 export default function Home() {
   return (
     <div>
@@ -16,7 +21,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="bg-white px-10 ">
+      <main className="bg-white px-10 md:px-20 lg:px-40">
         <section className="min-h-screen"> 
           <nav className="py-10 mb-12 flex justify-between" >
             <h1 className="text-xl font-light font-roboto ">nicoVolo</h1>
@@ -26,17 +31,22 @@ export default function Home() {
             </ul>
           </nav>
           <div className="text-center p -10">
-            <h2 className="text-5xl py-2 text-teal-600 font-light">Nico Volo</h2>
-            <h3 className="text-2xl py-2 text-gray-600">Frontend Developer</h3>
-            <p className="text-md py-5 leading-8 text-gray-800">Hello there! I'm Nico, a Junior Web Developer in both frontend and backend development, thirsty for learning and passionate about web technologies and building interesting projects. I love learning every day how to create open source projects and contribute to the community.
-</p>
+            <h2 className="text-5xl py-2 text-teal-600 font-light md:text-6xl">
+              Nico Volo
+            </h2>
+            <h3 className="text-2xl py-2 text-gray-600 md:text-3xl">
+              Frontend Developer
+            </h3>
+            <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto">
+              Hello there! I'm Nico, a Junior Web Developer in both frontend and backend development, thirsty for learning and passionate about web technologies and building interesting projects. I love learning every day how to create open source projects and contribute to the community.
+            </p>
           </div>
           <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
             <a className="hover:text-teal-500" href="#"><AiFillGithub/></a>
             <a className="hover:text-teal-500" href="#"><AiFillLinkedin/></a>
             <a className="hover:text-teal-500" href="#"><AiFillInstagram/></a>
           </div>
-          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden ">
+          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96">
             <Image src={deved} layout="fill" objectFit="cover"/>
           </div>
         </section>
@@ -50,7 +60,7 @@ export default function Home() {
             Also, for 2 years I have worked as programming teacher for kids, teaching them how to create awesome games using programming languages like <span className="text-teal-500 bold">JavaScript</span> or <span className="text-teal-500 bold">Python</span>. 
             </p>
           </div>
-          <div> 
+          <div className="lg:flex gap-10"> 
             <div className="text-center items-center shadow-lg p-10 rounded-xl my-10">
               <Image src={design} width={100} height={100} style={{margin:"auto"}} />
               <h3 className="text-lg font-medium pt-8 pb-2">Beautiful designs</h3>
@@ -79,7 +89,7 @@ export default function Home() {
               <Image src={consulting} width={100} height={100} style={{margin:"auto"}} />
               <h3 className="text-lg font-medium pt-8 pb-2">Search Engine Optimization</h3>
               <p className="py-2">
-              Properly build, tag and optimize the websites
+              Properly build, structure and tag the website components
               </p>
               <h4 className="py-4 text-teal-400">Search Engines love when the websites are</h4>
               <p className="text-gray-800 py-1">Lightweighted</p>
@@ -89,7 +99,37 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <section></section>
+        <section>
+          <div>
+            <h3 className="text-3xl py-1">Portfolio</h3>
+            <p className="text-md py-5 leading-8 text-gray-800">I provide SEO optimization, maintenance and creation of modern, fast and mobile friendly websites.
+            <br/>
+            I worked for 5 years as <span className="text-teal-500 bold">SEO specialist</span> for real estate agency, providing organic traffic and improving Google search positions of the website on the extremely competetive market, as well as speeding up website's speed and usability using latest solutions
+            <br/>
+            Also, for 2 years I have worked as programming teacher for kids, teaching them how to create awesome games using programming languages like <span className="text-teal-500 bold">JavaScript</span> or <span className="text-teal-500 bold">Python</span>. 
+            </p>
+          </div>
+          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+            <div className="basis-1/3 flex-1">
+              <Image src={web1} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image src={web2} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image src={web6} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image src={web4} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image src={web5} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image src={web3} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
+            </div>
+          </div>
+        </section>
       </main> 
     </div>
   )
