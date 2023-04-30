9ac4015041d8c8b032f89f27dfc5840ffa0ec90d
Nico <nickbazie@gmail.com>
2023-05-01 01:45:16 +0200

    Added fairy forest

--- react-portfolio/pages/index.js
+++ react-portfolio/pages/index.js
@@ -12,6 +12,7 @@ import web3 from '../public/web3.png';
 import web4 from '../public/web4.png';
 import web5 from '../public/web5.png';
 import web6 from '../public/web6.png';
+import fairy-forest from '../public/fairy-forest.png';
 import {useState} from 'react';
 
 export default function Home() {
@@ -115,7 +116,7 @@ export default function Home() {
           </div>
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
             <div className="basis-1/3 flex-1">
-              <Image src={web1} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
+              <a href="https://parallax-rey9.vercel.app/"><Image src={fairy-forest} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/></a>
             </div>
             <div className="basis-1/3 flex-1">
               <Image src={web2} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
