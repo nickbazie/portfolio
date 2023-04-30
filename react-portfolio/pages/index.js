26e5481c490c14df975211355024dc1081490865
Nico <nickbazie@gmail.com>
2023-05-01 01:54:42 +0200

    Update index.js

--- react-portfolio/pages/index.js
+++ react-portfolio/pages/index.js
@@ -6,7 +6,6 @@ import deved from '../public/dev-ed-wave.png';
 import design from '../public/design.png';
 import code from '../public/code.png';
 import consulting from '../public/consulting.png';
-import web1 from '../public/web1.png';
 import web2 from '../public/web2.png';
 import web3 from '../public/web3.png';
 import web4 from '../public/web4.png';
